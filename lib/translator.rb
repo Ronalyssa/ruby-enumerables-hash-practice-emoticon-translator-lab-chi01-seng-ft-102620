da914e6576cce626b7496c3323f38d7fa3bee5bc
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-10-09 22:33:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(path, emoticon)
   # code goes here
   load_library(path).each do |key, value|
     if value[:english] == emoticon
-      return key
+      return value
     end
   end
   return "Sorry, that emoticon was not found"
