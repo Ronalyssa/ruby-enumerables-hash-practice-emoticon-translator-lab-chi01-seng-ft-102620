f05cb1abcf0a20cdf66951a922c8e0560009a8bb
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-10-09 21:31:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,7 @@ def load_library(path)
   final_hash = {}
   YAML.load_file(path).each do |key, value|
     final_hash[key] = {}
+    final_hash[key][:english] = ""
   #binding.pry
   end
   final_hash
